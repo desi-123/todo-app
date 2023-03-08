e498e85724c8fe600386a9f29c201613be563838
desi-123 <desiemeng21m@gmail.com>
2023-03-08 12:52:25 -0800

    logo updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,23 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import logo from '../logo.jpg'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import todoapplogo from '../todoapplogo.png';
 
 const Header = () => {
   return (
-    <header className="header">
-      <Link to="/">
-        <img src={logo} alt="Focus360" className="header--logo" />
+    <header className='header'>
+      <Link to='/'>
+        <img src={todoapplogo} alt='Focus360' className='header--logo' />
       </Link>
-      <div className="header__list">
-        <Link className="header--link-home" to="/">
+      <div className='header__list'>
+        <Link className='header--link-home' to='/'>
           Home
         </Link>
-        <Link className="header--link-about" to="/about">
+        <Link className='header--link-about' to='/about'>
           About
         </Link>
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
